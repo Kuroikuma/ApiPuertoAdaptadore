b93815693a440096d0578a18f646abc4d2ed2755
ArleysGatica <gatica.arleys16@gmail.com>
2022-05-25 15:19:12 -0600

    adding script

---
+++ Scrip-Pizza.sql
@@ -0,0 +1,12 @@
+insert into Administrador values ('2d414320-edb2-41dd-9c87-c9b0fe7bc8ef', 'Carlos', 'carlitos@gmail.com', 3,'hurtado','admin.jpg',2545454,'M','12345c')
+select * from ingrediente
+insert into cliente values ('ca7af854-0538-4188-8845-0b84e497b1ca', 'Ulices','Hurtado','kuro08@gmail.com',215151,'cliente.jpg','M', 1 ,'ulices01')
+insert into Empleado values ('D82874A1-4476-4919-917D-C6EEC25A0217', 'Arleys', 'Gatica',15515,'M', 'arleys01.jpg', 'arleys01@gmail.com', 2,'arl123')
+insert into Root values ('e7a62b66-6143-4c79-92fc-c1c29f1b898d', 'Levi', 'pizza',3544,'levi09.jpg','M', 'levip@gmail.com', 4, 'l010120')
+insert into Categoria values('48c8c50d-5b40-45a0-9e85-77255f559ade','Pizza','Las mejres pizza')
+insert into Categoria values('5860AC3E-2AF3-40EF-B5BE-C3AD92ABBF75','bebidas','Las mejres bebidas')
+insert into Ingrediente values('e93bf852-640c-4673-8c95-6e5912089def','Salami',20,45,'./Content/Ingrediente/Salami.jpg','oz')
+insert into Ingrediente values('b62c4900-ab4b-4f8a-aa11-014bf1305765','Chiltoma',20,45,'./Content/Ingrediente/Chiltoma.jpg','ud')
+insert into Ingrediente values('660e805d-7b08-4f19-ada3-164ca168902a','Queso',20,45,'./Content/Ingrediente/Queso.jpg','oz')
+insert into Ingrediente values('b9de7d82-7e23-4a75-97d4-e23b8286cce8','Sal',10,20,'./Content/Ingrediente/sal.jpg','oz')
+insert into Ingrediente values('c35de520-609f-4d39-a1be-38d589edb032','Ajo',15,30,'./Content/Ingrediente/ajo.jpg','ud')
